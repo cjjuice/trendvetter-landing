95095de6e650d8774bd2cdc130908517c92b97da
C.J. Bordeleau <bordeleau.cj@gmail.com>
2013-06-12 17:16:52 -0400

    Add lead validations.

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,3 +1,8 @@
 class Lead < ActiveRecord::Base
   attr_accessible :company, :email, :name
+
+  validates_presence_of :name, :email, message: "can't be empty"
+  validates_uniqueness_of :email
+
+  validates :email, email: true
 end

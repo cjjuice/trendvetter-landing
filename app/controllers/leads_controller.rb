395b18a3b2e4ad4cdded15aeaf59692f487a1b90
C.J. Bordeleau <bordeleau.cj@gmail.com>
2013-06-12 16:50:29 -0400

    Update message styling.

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -6,9 +6,9 @@ class LeadsController < ApplicationController
   def create
     @lead = Lead.create(params[:lead])
     if @lead.save
-      redirect_to root_url, notice: "Thank you for yor interest in TrendVetter"
+      redirect_to root_url, notice: "Thank you for your interest in TrendVetter. We will contact you shortly."
     else
-      render "new"  
+      render "new"
     end
   end
 end
